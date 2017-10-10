db3bd9036cb83e7206197b9e3bc5bbb4d6e22eef
Sergey Kartashev <sergei.kartashev@yandex.ru>
2017-10-10 13:45:42 +0300

    added arm&disarm handlers

--- modules/photon/px4/Px4.c
+++ modules/photon/px4/Px4.c
@@ -1,5 +1,11 @@
 #include "photon/px4/Px4.Component.h"
 
+#include "photon/core/Logging.h"
+
+#define _PHOTON_FNAME "px4/Px4.c"
+
+extern void setArmed();
+
 void PhotonPx4_Init()
 {
 }
@@ -7,3 +13,17 @@ void PhotonPx4_Init()
 void PhotonPx4_Tick()
 {
 }
+
+PhotonError PhotonPx4_Arm()
+{
+    setArmed(true);
+    return PhotonError_Ok;
+}
+
+PhotonError PhotonPx4_Disarm()
+{
+    setArmed(false);
+    return PhotonError_Ok;
+}
+
+#undef _PHOTON_FNAME
