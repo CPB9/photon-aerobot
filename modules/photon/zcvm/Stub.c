ded4e6f1ca5e412bb2d5a09abedc999f5776486e
Sergey Kartashev <sergei.kartashev@yandex.ru>
2018-04-25 17:47:15 +0300

    zcvm stub

---
+++ modules/photon/zcvm/Stub.c
@@ -0,0 +1,51 @@
+#include "photongen/onboard/zcvm/Zcvm.Component.h"
+
+#define _PHOTON_FNAME "Zcvm.c"
+
+void PhotonZcvm_Init()
+{
+    _photonZcvm.slavesState.powerCom = false;
+    _photonZcvm.slavesState.powerFan1 = false;
+    _photonZcvm.slavesState.powerFan2 = false;
+
+    _photonZcvm.slavesState.powerComLastTime = 0;
+    _photonZcvm.slavesState.powerFan1LastTime = 0;
+    _photonZcvm.slavesState.powerFan2LastTime = 0;
+
+}
+
+void PhotonZcvm_Tick()
+{
+    const int warningTime = 1 * 1e3;
+
+    PhotonZcvmSlaveRunningState state;
+    state.powerCom = PhotonClk_GetTickTime() - _photonZcvm.slavesState.powerComLastTime < warningTime;
+    state.powerFan1 = PhotonClk_GetTickTime() - _photonZcvm.slavesState.powerFan1LastTime < warningTime;
+    state.powerFan2 = PhotonClk_GetTickTime() - _photonZcvm.slavesState.powerFan2LastTime < warningTime;
+    PHOTON_CRITICAL("%u", PhotonClk_GetTickTime() - _photonZcvm.slavesState.powerFan1LastTime);
+    if (_photonZcvm.slavesState.powerCom != state.powerCom ||
+        _photonZcvm.slavesState.powerFan1 != state.powerFan1 ||
+        _photonZcvm.slavesState.powerFan2 != state.powerFan2)
+    {
+        _photonZcvm.slavesState.powerCom = state.powerCom;
+        _photonZcvm.slavesState.powerFan1 = state.powerFan1;
+        _photonZcvm.slavesState.powerFan2 = state.powerFan2;
+        PhotonZcvm_QueueEvent_SlaveConnectionChanged(&_photonZcvm.slavesState);
+    }
+}
+
+void PhotonZcvm_UpdateSlaveMessageTime(PhotonZcvmSlaves slave)
+{
+    switch (slave)
+    {
+        case PhotonZcvmSlaves_PowerCom:
+            _photonZcvm.slavesState.powerComLastTime = PhotonClk_GetTickTime();
+            break;
+        case PhotonZcvmSlaves_PowerFan1:
+            _photonZcvm.slavesState.powerFan1LastTime = PhotonClk_GetTickTime();
+            break;
+        case PhotonZcvmSlaves_PowerFan2:
+            _photonZcvm.slavesState.powerFan2LastTime = PhotonClk_GetTickTime();
+            break;
+    }
+}
