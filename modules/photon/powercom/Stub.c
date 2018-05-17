583799e7ceac3bd224b0d83dff41768726aba01d
dndanik <dndanik@gmail.com>
2018-05-17 19:35:05 +0300

    Powercom stubs

--- modules/photon/powercom/Stub.c
+++ modules/photon/powercom/Stub.c
@@ -1,4 +1,14 @@
 #ifdef PHOTON_STUB
-//#include "photon/esc/Esc.Component.h"
+#include "photongen/onboard/powercom/Powercom.Component.h"
+
+PhotonError PhotonPowercom_ExecCmd_EnableChannel(PhotonPowercomChannel chan)
+{
+    return PhotonError_NotImplemented;
+}
+
+PhotonError PhotonPowercom_ExecCmd_DisableChannel(PhotonPowercomChannel chan)
+{
+    return PhotonError_NotImplemented;
+}
 
 #endif
