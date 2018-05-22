995f641b0b91a226de11e377442245a8b5ff38b8
Dmitry Voronkin <dmitry@voronkin.ru>
2018-05-22 19:06:46 +0300

    Zlidar stubs added.

---
+++ modules/photon/zlidar/Stubs.c
@@ -0,0 +1,23 @@
+#include "photongen/onboard/zlidar/Zlidar.Component.h"
+
+#ifdef PHOTON_STUB
+
+void PhotonZlidar_Init(){}
+void PhotonZlidar_Tick(){}
+
+/*cmd handlers*/
+PhotonError PhotonZlidar_ExecCmd_UpdateInfo(PhotonZlidarFrames frameCode) { return PhotonError_NotImplemented; }
+PhotonError PhotonZlidar_ExecCmd_Start() { return PhotonError_NotImplemented; }
+PhotonError PhotonZlidar_ExecCmd_Stop() { return PhotonError_NotImplemented; }
+PhotonError PhotonZlidar_ExecCmd_SetAccumulation(PhotonZlidarAccumulations accumulation) { return PhotonError_NotImplemented; }
+PhotonError PhotonZlidar_ExecCmd_SetOversampling(PhotonZlidarOversamplings oversampling) { return PhotonError_NotImplemented; }
+PhotonError PhotonZlidar_ExecCmd_SetPoints(uint16_t points) { return PhotonError_NotImplemented; }
+PhotonError PhotonZlidar_ExecCmd_SetThresholdOffset(float sensitivity) { return PhotonError_NotImplemented; }
+PhotonError PhotonZlidar_ExecCmd_SetSmoothing(int8_t smoothing) { return PhotonError_NotImplemented; }
+PhotonError PhotonZlidar_ExecCmd_SetLedPower(PhotonZlidarLedPowers power) { return PhotonError_NotImplemented; }
+PhotonError PhotonZlidar_ExecCmd_SetAcquisitionOption(uint8_t acquisition) { return PhotonError_NotImplemented; }
+PhotonError PhotonZlidar_ExecCmd_SetAutoFrame(uint16_t frame) { return PhotonError_NotImplemented; }
+PhotonError PhotonZlidar_ExecCmd_SetAutoEcho(uint16_t echo) { return PhotonError_NotImplemented; }
+PhotonError PhotonZlidar_ExecCmd_SetOperationMode(PhotonZlidarOperationModeWrite mode) { return PhotonError_NotImplemented; }
+
+#endif
